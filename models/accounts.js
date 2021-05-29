fc58d81ab1853b8a6009672b246a4a5dfc6dc69e
WilliamTsou818 <cccc1589@gmail.com>
2021-05-29 14:23:51 +0800

    feat : add account seed data

--- models/accounts.js
+++ models/accounts.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const accountSchema = new Schema({
+  firstName : {
+    type : String,
+    required : true
+  },
   email : {
     type : String,
     required : true
---
+++ models/seeds/toSeeder.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+const Account = require('../accounts')
+const users = [
+  {
+    firstName: 'Tony',
+    email: 'tony@stark.com',
+    password: 'iamironman'
+  },
+  {
+    firstName: 'Steve',
+    email: 'captain@hotmail.com',
+    password: 'icandothisallday'
+  },
+  {
+    firstName: 'Peter',
+    email: 'peter@parker.com',
+    password: 'enajyram'
+  },
+  {
+    firstName: 'Natasha',
+    email: 'natasha@gamil.com',
+    password: '*parol#@$!'
+  },
+  {
+    firstName: 'Nick',
+    email: 'nick@shield.com',
+    password: 'password'
+  }
+]
+
+mongoose.connect('mongodb://localhost/account', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongoDB error!')
+})
+
+db.once('open', () => {
+  console.log('mongoDB connected!')
+  users.forEach(user => {
+    Account.create({
+      firstName : user.firstName,
+      email : user.email,
+      password : user.password
+    })
+  })
+  console.log('done!')
+})
